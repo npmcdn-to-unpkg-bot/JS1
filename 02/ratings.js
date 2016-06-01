52da11bf634863c159f5f6d9c33ce24e02edbf4b
Amy Simmons <amy__simmons@hotmail.com>
2016-06-01 17:10:36 +1000

    using map for ages exercise

--- 02/ratings.js
+++ 02/ratings.js
@@ -25,18 +25,36 @@ var ages = [1, 23, 8, 12, 16];
 var ratings = ['G', 'PG', 'M', 'MA'];
 var minAgeForRating = [0, 9, 12, 15];
 
-ages.forEach(function(age){
+ages.forEach(function(age, i){
   var age = age;
   var canSee = [];
-  var i = 0;
 
-  minAgeForRating.forEach(function(minAge){
+  minAgeForRating.forEach(function(minAge, i){
     if(age >= minAge){
       canSee.push(ratings[i]);
     }
-    i++;
   });
 
   console.log(age);
+  console.log(canSee);
+});
+
+
+/*
+.map()` returns a new array where each value in the original array is replaced with
+what you return from the `function`. Can you think how you'd go from what you've got
+to an index in the ratings array, and return that from map?
+*/
+
+ratings.forEach(function(rating, i){
+  console.log(rating);
+
+  var canSee = [];
+  ages.map(function(age){
+    if( age >= minAgeForRating[i] ){
+      canSee.push(age);
+    }
+  });
+
   console.log(canSee);
 });
