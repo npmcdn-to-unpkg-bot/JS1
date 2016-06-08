09cddc1e3ff274242433f3eb3433b4ddc4d1e341
amysimmons <amy__simmons@hotmail.com>
2016-06-08 12:03:50 +1000

    sticky notes solution

---
+++ 03/sticky-notes/app.js
@@ -0,0 +1,31 @@
+function stickyNoteNumber(){
+	return document.getElementsByClassName('box').length + 1;
+}
+
+function createStickyNote(colour, message){
+	var stickyNote = document.createElement('div');
+	var stickyNoteMessge = document.createElement('span');
+	
+	stickyNote.className = "box";
+	stickyNote.style.backgroundColor = colour;
+	stickyNoteMessge.innerHTML = stickyNoteNumber() + ". " + message;
+	stickyNote.appendChild(stickyNoteMessge);
+	
+	addStickyNoteToPage(stickyNote);
+}
+
+function addStickyNoteToPage(stickyNote){
+	document.querySelector('.sticky-notes').appendChild(stickyNote);
+}
+
+document.querySelector('#submit').onclick = function(e){
+	e.preventDefault();
+
+	var colour = document.querySelector('#colour').value;
+	var message = document.querySelector('#message').value;
+
+	createStickyNote(colour, message);
+
+	document.querySelector('#colour').value = "";
+	document.querySelector('#message').value = "";
+}
