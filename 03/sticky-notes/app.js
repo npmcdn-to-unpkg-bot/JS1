9697cf740038d4e684dcc9178b4e8e05b84cd785
Amy Simmons <amy__simmons@hotmail.com>
2016-06-15 18:22:35 +1000

    improved html

--- 03/sticky-notes/app.js
+++ 03/sticky-notes/app.js
@@ -5,12 +5,12 @@ function stickyNoteNumber(){
 function createStickyNote(colour, message){
 	var stickyNote = document.createElement('div');
 	var stickyNoteMessge = document.createElement('span');
-	
+
 	stickyNote.className = "box";
 	stickyNote.style.backgroundColor = colour;
 	stickyNoteMessge.innerHTML = stickyNoteNumber() + ". " + message;
 	stickyNote.appendChild(stickyNoteMessge);
-	
+
 	addStickyNoteToPage(stickyNote);
 }
 
@@ -18,9 +18,7 @@ function addStickyNoteToPage(stickyNote){
 	document.querySelector('.sticky-notes').appendChild(stickyNote);
 }
 
-document.querySelector('#submit').onclick = function(e){
-	e.preventDefault();
-
+document.querySelector('button').onclick = function(e){
 	var colour = document.querySelector('#colour').value;
 	var message = document.querySelector('#message').value;
 
