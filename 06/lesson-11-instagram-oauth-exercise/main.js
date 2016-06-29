1435974c71a46ecf088700e5b87990c9da34db92
amysimmons <amy__simmons@hotmail.com>
2016-06-29 16:58:20 +1000

    image fetching from insta api and rendering on page

--- 06/lesson-11-instagram-oauth-exercise/main.js
+++ 06/lesson-11-instagram-oauth-exercise/main.js
@@ -10,22 +10,43 @@
   var forecastBasUrl = 'https://crossorigin.me/https://api.forecast.io/forecast/'
   var instagramBasUrl = 'https://crossorigin.me/https://api.instagram.com/v1/'
   var container = document.querySelector('#container')
-  var state = {}
+  var state = {
+    accessToken: window.location.hash.split('=')[1] ? window.location.hash.split('=')[1] : false,
+    recentMediaUrl: ''
+  }
+
+  //var accessToken = window.location.hash.split('=')[1]
 
   if (false /* TODO: did user authorize? */) {
     renderLogin(state, container)
   } else {
+    var userMediaUrl = `https://crossorigin.me/https://api.instagram.com/v1/users/self/media/recent/?access_token=${state.accessToken}`
 
-    // TODO: Make your fetch calls here
-
-    renderImages(state, container)
+    fetch(userMediaUrl).then((response) => {
+      return response.json()
+    }).then((dataAsJson) => {
+      state.recentMediaUrl = dataAsJson.data[0].images.standard_resolution.url 
+      renderImages(state, container)
+    })
   }
 
   function renderLogin(data, into) {
-    // TODO: Add the template
+    into.innerHTML = `
+      <h2>What was the weather when you snapped your shot?</h2>
+      <form action="https://api.instagram.com/oauth/authorize/?client_id=a965a951fc3d4a38a4b607a0fafad38e&redirect_uri=http://localhost:3000&response_type=token" method="post">
+        <button type="submit">Login to Instagram</button>
+      </form>
+    `
   }
 
   function renderImages(data, into) {
-    // TODO: Add the template
+    into.innerHTML = `
+      <h2>Hey, here's the weather during your shot!</h2>
+      <div class="instaweather">
+        <img src=${data.recentMediaUrl} />
+        The weather was: TODO
+      </div>
+    `
   }
 })()
+
